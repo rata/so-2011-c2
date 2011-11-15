60304621391576d2b32c82a064317cc9a03189ef
rodrigocc <rodrigocc@gmail.com>
2011-11-15 21:53:33 +0000

    Agregar criterio de parada, agregar comentarios aclaratorios y borrar prints

--- tp3/src/srv.c
+++ tp3/src/srv.c
@@ -19,6 +19,19 @@ void send_broadcast_req()
 	
 }
 
+void send_broadcast_finished()
+{
+	for (int i = 0; i < n/2; i++) {
+		int srv = i * 2;
+		
+		if (srv == me)
+			continue;
+		// XXX: esta bien usar el MPI_Send (teniendo en cuenta que puede bloquear hasta que el otro lado haga Recv) ?
+		MPI_Send(NULL, 0, MPI_INT, srv, SRV_FINISHED, COMM_WORLD);
+	}
+}
+
+
 void servidor(int mi_cliente)
 {
 	MPI_Status status;
@@ -27,6 +40,7 @@ void servidor(int mi_cliente)
 	int outstanding_reply = (n/2) - 1;
 	int hay_pedido_local = FALSE;
 	int listo_para_salir = FALSE;
+	int clients_alive = n/2;
 	
 	/* Array para guardar los reply que "postergamos" a los servidores
 	 * Como hay n servidores y 2n procesos, la cantidad de servidores es la cantidad de procesos sobre 2
@@ -56,8 +70,6 @@ void servidor(int mi_cliente)
 			our_seq_num = max_seq_num + 1;
 			send_broadcast_req();
 			
-			//debug("Dándole permiso");
-			//MPI_Send(NULL, 0, MPI_INT, mi_cliente, TAG_OTORGADO, COMM_WORLD);
 			break;
 		
 		case TAG_LIBERO:
@@ -67,7 +79,6 @@ void servidor(int mi_cliente)
 			hay_pedido_local = FALSE;
 			outstanding_reply = (n/2) - 1;
 
-			
 			/* Procesar los reply_deferred */
 			for (int i = 0; i < n/2; i++) {
 				int srv = i * 2;
@@ -82,20 +93,26 @@ void servidor(int mi_cliente)
 		case TAG_TERMINE:
 			assert(origen == mi_cliente);
 			debug("Mi cliente avisa que terminó");
-			// No salimos del while porque sino:
-			// los otros servidores esperan nuestra respuesta, pero no va a llegar nunca. Porque si salimos del while, morimos
-			// TODO: manejar el caso de que termino
-			//listo_para_salir = TRUE;
-			
+
+			// Si mi cliente termino, hay un cliente menos
+			clients_alive--;
+
+			// Le aviso al resto de los servidores que hay un
+			// cliente menos, asi actualizan la variable de
+			// cantidad de clientes vivos
+			send_broadcast_finished();
+
+			// Si mi cliente era el ultimo, estoy listo para salir
+			if (clients_alive == 0)
+				listo_para_salir = TRUE;
+		
 			break;
 			
 		case SRV_REQ:
 			debug("llego SRV_REQ");
-			//printf("buf es %d\n", buf);
 			int k = buf;
 			max_seq_num = (k > max_seq_num) ? k: max_seq_num;
 
-			printf("max_seq es %d\n", max_seq_num);
 			if (hay_pedido_local && (k > our_seq_num || (k == our_seq_num && origen > me))) {
 				int srv = origen/2;
 				assert(srv < n/2);
@@ -111,14 +128,22 @@ void servidor(int mi_cliente)
 			debug("llego SRV_REPLY");
 			outstanding_reply -= 1;
 			
+			// Me llegaron todas las respuestas de los servidores
 			if (outstanding_reply == 0) {
 				debug("Dándole permiso");
 				MPI_Send(NULL, 0, MPI_INT, mi_cliente, TAG_OTORGADO, COMM_WORLD);
-			} else {
-				printf("faltan %d replies", outstanding_reply);
 			}
 		
 			break;
+
+		case SRV_FINISHED:
+
+			// Termino el cliente de algun servidor (no el mio). Asique queda un cliente menos
+			// PEro si ese cliente era el ultimo, estoy listo para salir.
+			clients_alive--;
+			if (clients_alive == 0)
+				listo_para_salir = TRUE;
+
 
 		default:
 			debug("Tag desconocido!");
@@ -126,6 +151,5 @@ void servidor(int mi_cliente)
 		}
 	}
 
-	printf("servidor finalizado\n");
 	free(reply_deferred);
 }
